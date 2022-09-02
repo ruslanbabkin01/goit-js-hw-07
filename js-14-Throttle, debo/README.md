cfc96284b249fa975f9a06d68c678eb4a6eef375
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-09-03 00:00:11 +0300

    done all tasks

--- js-14-Throttle, debo/README.md
+++
@@ -1,7 +0,0 @@
-# js-22
-
-- Chatty events (болтливые события)
-- Приёмы throttle и debounce с lodash
-- [Ленивая загрузка изображений](https://web.dev/native-lazy-loading/)
-  - Нативная с атрибутом loading
-  - Библиотека lazysizes
--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне.Посмотри демо видео работы галереи.
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+const galeryDiv = document.querySelector(".gallery");
 
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox.Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min)
-//  файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую разметку модального окна с изображением из примеров
-//  библиотеки basicLightbox.
-//    Разметка элемента галереи
-// Ссылка на оригинальное изображение должна храниться в data - атрибуте source на элементе < img >, и указываться в href ссылки.Не добавляй другие 
-// HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
-{/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+function createItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */}
-//    Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. 
-//    Запрети это поведение по умолчанию.
-// Закрытие с клавиатуры
-// ВНИМАНИЕ
-// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
-// Добавь закрытие модального окна по нажатию клавиши Escape.Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. 
-// У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></div>`;
+    })
+    .join("");
+}
+
+galeryDiv.addEventListener("click", onDivImagesClick);
+
+const itemsMarkup = createItemsMarkup(galleryItems);
+
+galeryDiv.innerHTML = itemsMarkup;
+
+function onDivImagesClick(evt) {
+  const isImage = evt.target.classList.contains("gallery__image");
+
+  if (!isImage) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  galeryDiv.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+
+  // Заборона відкриття по посиланню
+  blockAction(evt);
+}
+
+function blockAction(evt) {
+  evt.preventDefault();
+}
+
 console.log(galleryItems);
